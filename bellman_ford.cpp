f8d1b2b13116a327cde896092d1c03d7cf3652e3
Vsevolod <1cheaterok1@gmail.com>
2018-04-04 07:31:48 +0300

    Добавляет параллелизм

--- bellman_ford.cpp
+++ bellman_ford.cpp
@@ -3,8 +3,9 @@
 #include <iostream>
 #include <fstream>
 #include <sstream>
-#include <algorithm>
 #include <chrono>
+#include <algorithm>
+#include <unistd.h>
 
 
 using namespace std;
@@ -17,9 +18,9 @@ using namespace std;
  */
 
 struct edge {
-	int a, b, cost;
+    int a, b, cost;
 };
- 
+
 /*
  * n - кол-во вершин
  * m - кол-во рёбер
@@ -34,76 +35,79 @@ const int INF = numeric_limits<int>::max();
 
 vector<int> parseNextRow(istream& str)
 {
-    vector<int>   result;
-    string                line;
+    vector<int> result;
+    string line;
     getline(str,line);
 
-    stringstream          lineStream(line);
-    string                cell;
+    stringstream lineStream(line);
+    string cell;
 
     while(getline(lineStream, cell, ','))
-    {
         result.push_back(stoi(cell));
-    }
-    
+
     return result;
 }
 
 
 void solve() {
-	vector<int> d (n, INF);
-	d[v] = 0;
-	vector<int> p (n, -1);
+    vector<int> d (n, INF);
+    d[v] = 0;
+    vector<int> p (n, -1);
 
-    	// ЗАСЕКАЕМ ВРЕМЯ
-    	auto startTime = std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::system_clock::now().time_since_epoch()).count();
+    // ЗАСЕКАЕМ ВРЕМЯ
+    auto startTime = std::chrono::high_resolution_clock::now();
 
-	for (int i=0; i<n-1; i++) {
-		for (int j=0; j<m; j++)
-			if (d[e[j].a] < INF)
-				if (d[e[j].b] > d[e[j].a] + e[j].cost) {
-					d[e[j].b] = d[e[j].a] + e[j].cost;
-					p[e[j].b] = e[j].a;
-				}
-	}
+    for (int i=0; i<n-1; i++) {
+    #pragma omp parallel for
+        for (int j=0; j<m; j++)
+            if (d[e[j].a] < INF) {
+                sleep(1);
+                if (d[e[j].b] > d[e[j].a] + e[j].cost) {
+                    d[e[j].b] = d[e[j].a] + e[j].cost;
+                    p[e[j].b] = e[j].a;
+                }
+            }
+    }
 
-    auto stopTime = std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::system_clock::now().time_since_epoch()).count();
-    cout << "Time passed: " << stopTime - startTime << endl;
+    auto stopTime = std::chrono::high_resolution_clock::now();
+    std::chrono::duration<double> elapsed = stopTime - startTime;
+    cout << "Time passed: " << elapsed.count() << endl;
 
     if (d[t] == INF)
-		cout << "No path from " << v << " to " << t << ".";
-	else {
-		vector<int> path;
-		for (int cur=t; cur!=-1; cur=p[cur])
-			path.push_back (cur);
-		reverse (path.begin(), path.end());
+        cout << "No path from " << v << " to " << t << "." << endl;
+    else {
+        vector<int> path;
+        for (int cur=t; cur!=-1; cur=p[cur])
+            path.push_back(cur);
+        reverse (path.begin(), path.end());
 
-		cout << "Path from " << v << " to " << t << ": ";
-		for (size_t i=0; i<path.size(); ++i)
-			cout << path[i] << ' ';
-	}
+        cout << "Path from " << v << " to " << t << ": " << endl;
+        for (size_t i=0; i<path.size(); ++i)
+            cout << path[i] << ' ';
+        cout << endl;
+    }
 }
 
 
 int main() {
-        ifstream input_file;
+    ifstream input_file;
 
-        input_file.open("input", ios::out);
-        if (!input_file.is_open()) {
-                cout << "Could not open input file." << endl;
-                return -1;
-        }
+    input_file.open("input", ios::out);
+    if (!input_file.is_open()) {
+        cout << "Could not open input file." << endl;
+        return -1;
+    }
 
-        auto task_info = parseNextRow(input_file);
-        n = task_info[0]; m = task_info[1]; v = task_info[2]; t = task_info[3];
+    auto task_info = parseNextRow(input_file);
+    n = task_info[0]; m = task_info[1]; v = task_info[2]; t = task_info[3];
 
-        while (!input_file.eof()) {
-                auto result = parseNextRow(input_file);
-                e.push_back({result[0], result[1], result[2]});
-        }
+    while (!input_file.eof()) {
+        auto result = parseNextRow(input_file);
+        e.push_back({result[0], result[1], result[2]});
+    }
 
-        solve();
+    solve();
 
-        return 0;
+    return 0;
 }
 
